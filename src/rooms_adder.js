18923dbb91f18249787674ccbb075b1a5802c1d0
Hanscau <hanscau@hotmail.com>
2020-02-29 16:07:52 +0800

    Created room adder

--- src/rooms_adder.js
+++ src/rooms_adder.js
@@ -18,37 +18,17 @@ roomDescriptions = [
   "For those lovely couple who want to have some sexy fun fun time at night. Also recommended for the our sad single customer who have more cash to spend. Tissues are complementary, towels not included"
 ];
 
-var i = 4;
+var roomCardRaw = `            <div class="room_card" id="room_card">
+<img src="img/rooms/room_1.jpg" alt="" class="room_img">
+<h2 class="room_name">Super Single</h2>
+<p class="room_content">Only for our sad single customer. Not recommended for a group bigger than 1.
+    Towels for crying not included</p>
+</div>`;
 
-roomDescriptionsUI.innerHTML = roomDescriptions[i];
-roomNameUI.innerHTML = roomName[i];
-
-function change_room(n) {
-  i += n;
-
-  //Stops negative overflow
-  if (i < 0) {
-    i = 4;
-  }
-
-  slideshowContainer.style.opacity = 0;
-  roomNameUI.style.opacity = 0;
-  roomDescriptionsUI.style.opacity = 0;
-
-  //Index starts at 1
-  var roomIndex = i % 5;
-
-  console.log(roomIndex);
-
-  //Change image after 0.2s transition
-  setTimeout(() => {
-    slideshowContainer.style.backgroundImage =
-      "url(../img/rooms/room_" + roomIndex + ".jpg";
-    roomNameUI.innerHTML = roomName[roomIndex];
-    roomDescriptionsUI.innerHTML = roomDescriptions[roomIndex];
-
-    slideshowContainer.style.opacity = 1;
-    roomNameUI.style.opacity = 1;
-    roomDescriptionsUI.style.opacity = 1;
-  }, 200);
+for (var i = 0; i < 5; i++) {
+  var roomCardUI = $(roomCardRaw);
+  roomCardUI.children("img").attr("src", "img/rooms/room_" + i + ".jpg");
+  roomCardUI.children("h2").text(roomName[i]);
+  roomCardUI.children("p").text(roomDescriptions[i]);
+  $("#rooms_container").append(roomCardUI);
 }
