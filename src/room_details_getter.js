e2c9d79a2ab0ee5dfc2ee7849e288cb611b30c57
Hanscau <hanscau@hotmail.com>
2020-03-04 22:42:49 +0800

    Added icons

--- src/room_details_getter.js
+++ src/room_details_getter.js
@@ -2,5 +2,10 @@ var roomIndex = window.location.search.charAt(3);
 
 $("#title").text(roomName[roomIndex] + " | Golden Lake Hotel");
 $("#room_name").text(roomName[roomIndex]);
+
+$("#room_size").text(roomSpecifics["room_size"][roomIndex]);
+$("#room_view").text(roomSpecifics["room_view"][roomIndex]);
+$("#bed_size").text(roomSpecifics["bed_size"][roomIndex]);
+
 $("#room_description").text(roomDescriptions[roomIndex]);
 $("#main_img").attr("src", "img/rooms/room_" + roomIndex + ".jpg");
