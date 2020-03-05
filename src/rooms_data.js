d36db70ffd6b77b22dd5cde138d4776dd6bac29b
Hanscau <hanscau@hotmail.com>
2020-03-05 10:56:13 +0800

    Redone room details and content

--- src/rooms_data.js
+++ src/rooms_data.js
@@ -1,24 +1,24 @@
-roomName = ["Standard", "Superior", "Junior Suite", "Executive Suite"];
+roomName = ["Executive Suite", "Junior Suite", "Superior", "Standard"];
 
 roomDescriptions = [
-  "Only for our sad single customer. Not recommended for a group bigger than 1. Towels for crying not included",
-  'For those who are married but manages to make her mad. Feel free to ask for a paid upgrade to "I\'m a Queen" room. Towels not included',
-  "Families going out for a family fun time of running around and having a good experience. Also recommended for a couple going out with their photographer or friend or that one third wheel guy. Wheels not included",
-  "For those lovely couple who want to have some sexy fun fun time at night. Also recommended for the our sad single customer who have more cash to spend. Tissues are complementary, towels not included"
+  "Our best offering that will guarantee a good nights rest. Comes with a single bed and a double bed, great for families or those who just want to pamper themselves after a tiring day at work. Facilities includes private bathroom, sofa and a flat screen LCD TV",
+  "Best recommendation for families, comes with a single bed and a double bed. Facilities includes private bathroom, sofa and a flat screen LCD TV",
+  "Our Superior rooms comes with either 2 single bed or 1 double bed. It is the perfect room for a simple calming rest after a day of travelling around. Comes with a private toilet and a public bathroom.",
+  "Our Standard room comes with either 2 single bed or 1 double bed. Our offering for those travelling across the country looking for a place to rest. A clean room with a private toilet and a public bathroom."
 ];
 
 roomSpecifics = {
   room_size: [
-    "20 m²/215 ft²",
-    "19 m²/205 ft²",
     "139 m²/1496 ft²",
-    "139 m²/1496 ft²"
+    "139 m²/1496 ft²",
+    "19 m²/205 ft²",
+    "20 m²/215 ft²"
   ],
-  room_view: ["Lake view", "Lake view", "Garden view", "Lake view"],
+  room_view: ["Lake view", "Garden view", "Lake view", "Lake view"],
   bed_size: [
-    "2 single bed or 1 double bed",
-    "2 single bed or 1 double bed",
     "2 single bed and 1 double bed",
-    "2 single bed and 1 double bed"
+    "2 single bed and 1 double bed",
+    "2 single bed or 1 double bed",
+    "2 single bed or 1 double bed"
   ]
 };
