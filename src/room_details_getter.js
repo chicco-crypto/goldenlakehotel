c191c1fb58c0cce6db1499ae413408ceca419e90
Hanscau <hanscau@hotmail.com>
2020-04-07 15:32:15 +0800

    Added secondary image function

--- src/room_details_getter.js
+++ src/room_details_getter.js
@@ -8,4 +8,27 @@ $("#room_view").text(roomSpecifics["room_view"][roomIndex]);
 $("#bed_size").text(roomSpecifics["bed_size"][roomIndex]);
 
 $("#room_description").text(roomDescriptions[roomIndex]);
-$("#main_img").attr("src", "img/rooms/room_" + roomIndex + ".jpg");
+
+for (var i = 0; i < noOfImage[roomIndex]; i++) {
+  var pathToImage = "img/rooms/" + roomRawName[roomIndex] + "/img";
+
+  var imageElem = $("<img>");
+  imageElem.attr("src", pathToImage + (i + 1) + ".jpg");
+  imageElem.attr("id", "image" + (i + 1));
+  imageElem.attr("onclick", "setMainImg(this)");
+  imageElem.addClass("secondary_img");
+
+  $(".room_gallery").append(imageElem);
+}
+
+var currentSecondaryRoom = "image1";
+
+$("#" + currentSecondaryRoom).addClass("active");
+$("#main_img").attr("src", "img/rooms/" + roomRawName[roomIndex] + "/img1.jpg");
+
+var setMainImg = (elem) => {
+  $("#main_img").attr("src", elem.src);
+  $("#" + currentSecondaryRoom).removeClass("active");
+  currentSecondaryRoom = elem.id;
+  elem.classList.add("active");
+};
--- src/rooms_data.js
+++ src/rooms_data.js
@@ -1,24 +1,33 @@
-roomName = ["Executive Suite", "Junior Suite", "Superior", "Standard"];
+var roomName = ["Executive Suite", "Junior Suite", "Superior", "Standard"];
 
-roomDescriptions = [
+var roomRawName = [
+  "executive_suite",
+  "junior_suite",
+  "superior_suite",
+  "standard_suite",
+];
+
+var noOfImage = [5, 5, 4, 3];
+
+var roomDescriptions = [
   "Our best offering that will guarantee a good nights rest. Comes with a single bed and a double bed, great for families or those who just want to pamper themselves after a tiring day at work. Facilities includes private bathroom, sofa and a flat screen LCD TV",
   "Best recommendation for families, comes with a single bed and a double bed. Facilities includes private bathroom, sofa and a flat screen LCD TV",
   "Our Superior rooms comes with either 2 single bed or 1 double bed. It is the perfect room for a simple calming rest after a day of travelling around. Comes with a private toilet and a public bathroom.",
-  "Our Standard room comes with either 2 single bed or 1 double bed. Our offering for those travelling across the country looking for a place to rest. A clean room with a private toilet and a public bathroom."
+  "Our Standard room comes with either 2 single bed or 1 double bed. Our offering for those travelling across the country looking for a place to rest. A clean room with a private toilet and a public bathroom.",
 ];
 
-roomSpecifics = {
+var roomSpecifics = {
   room_size: [
     "139 m²/1496 ft²",
     "139 m²/1496 ft²",
     "19 m²/205 ft²",
-    "20 m²/215 ft²"
+    "20 m²/215 ft²",
   ],
   room_view: ["Lake view", "Garden view", "Lake view", "Lake view"],
   bed_size: [
     "2 single bed and 1 double bed",
     "2 single bed and 1 double bed",
     "2 single bed or 1 double bed",
-    "2 single bed or 1 double bed"
-  ]
+    "2 single bed or 1 double bed",
+  ],
 };
